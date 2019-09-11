44fd11127f36286f0ee93f1e369ffafc1eb3aea6
Artur Voll <info@pcservice-voll.de>
2019-09-12 00:28:33 +0200

    - phpcs and phpunit for development

---
+++ tests/src/WpRestRegistrationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Created by Media-Store.net
+ * User: Artur
+ * Date: 12.09.2019
+ * Time: 00:10
+ */
+class WpRestRegistrationTest extends TestCase
+{
+
+}
